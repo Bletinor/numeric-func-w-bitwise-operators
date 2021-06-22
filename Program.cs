ce777cd516fd48b139394a8283de56e4b21bdc38
Yeuris <yjtv@protonmail.com>
2021-06-21 20:34:17 -0400

    añade el programa inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace numeric_func_w_bitwise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
